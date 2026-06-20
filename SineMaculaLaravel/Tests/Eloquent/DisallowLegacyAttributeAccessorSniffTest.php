ad51da071e899b0817740c9f047c1ca915682869
Ben Carey <bdmc@sinemacula.co.uk>
2026-06-20 19:15:46 -0400

    feat(phpcs): disallow legacy Eloquent attribute accessors
    Flag getXAttribute()/setXAttribute() methods (superseded by
    Attribute::make()); the base getAttribute()/setAttribute() are left alone.

---
+++ SineMaculaLaravel/Tests/Eloquent/DisallowLegacyAttributeAccessorSniffTest.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace SineMaculaLaravel\Tests\Eloquent;
+
+use PHPUnit\Framework\Attributes\CoversNothing;
+use SineMaculaLaravel\Tests\AbstractSniffTestCase;
+
+/**
+ * Tests for the legacy attribute accessor sniff.
+ *
+ * @author      Ben Carey <bdmc@sinemacula.co.uk>
+ * @copyright   2026 Sine Macula Limited
+ *
+ * @internal
+ */
+#[CoversNothing]
+final class DisallowLegacyAttributeAccessorSniffTest extends AbstractSniffTestCase
+{
+    /**
+     * getXAttribute/setXAttribute methods are flagged; getAttribute and other
+     * methods are not.
+     *
+     * @return void
+     */
+    public function testFlagsLegacyAccessors(): void
+    {
+        $this->assertErrorsOnLines('DisallowLegacyAttributeAccessor.inc', [7, 12]);
+    }
+}
